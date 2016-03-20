0dc22648c2fb545a7b010e7cd336bad9628d1ddc
Waseem Morsi <wasymur@gmail.com>
2016-03-20 15:12:05 +0200

    change settings

--- backend/meshop/settings.py
+++ backend/meshop/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'meshop',
         'USER': 'root',
-        'PASSWORD': 'classy',
+        'PASSWORD': 'wasyclassy',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
