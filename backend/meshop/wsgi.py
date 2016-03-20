2634b034e790ba2dea9ef887214f92b58342e2d0
Waseem Morsi <wasymur@gmail.com>
2016-03-20 15:50:02 +0200

    wsgi config

--- backend/meshop/wsgi.py
+++ backend/meshop/wsgi.py
@@ -7,10 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meshop.settings")
+
+#application = get_wsgi_application()
+
+
 import os
-
+import sys
+import site
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/var/www/.virtualenvs/meshopvenv/local/lib/python2.7/site-packages')
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/var/www/meshop')
+sys.path.append('/var/www/meshop/meshop')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'meshop.settings'
+# Activate your virtual env
+activate_env=os.path.expanduser("/var/www/.virtualenvs/virtualenvs/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meshop.settings")
-
 application = get_wsgi_application()
