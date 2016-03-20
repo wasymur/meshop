9a6cace1c4081d086ba655f7e9a6fc89860b1851
Waseem Morsi <wasymur@gmail.com>
2016-03-20 14:54:04 +0200

    update settings file

--- backend/meshop/settings.py
+++ backend/meshop/settings.py
@@ -77,13 +77,22 @@ WSGI_APPLICATION = 'meshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'meshop',
+        'USER': 'root',
+        'PASSWORD': 'classy',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
